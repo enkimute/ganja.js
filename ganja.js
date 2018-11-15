7fb5274a5f07ac74bc7847b016a7779615528a07
enkimute <enkimute@gmail.com>
2018-11-15 16:01:52 +0100

    bugfix sizing graded canvas

--- ganja.js
+++ ganja.js
@@ -581,7 +581,7 @@
       static graphGL2(f,options) {
       // Create canvas, get webGL2 context.
         var canvas=document.createElement('canvas'); canvas.style.width=options.width||''; canvas.style.height=options.height||''; canvas.style.backgroundColor='#EEE';
-        if (options.width && options.width.match(/px/i)) canvas.width = parseFloat(options.width); if (options.height && options.height.match(/px/i)) canvas.height = parseFloat(options.height);
+        if (options.width && options.width.match && options.width.match(/px/i)) canvas.width = parseFloat(options.width); if (options.height && options.height.match && options.height.match(/px/i)) canvas.height = parseFloat(options.height);
         var gl=canvas.getContext('webgl2',{alpha:options.alpha||false,preserveDrawingBuffer:true,antialias:true,powerPreference:'high-performance'}); 
         gl.clearColor(240/255,240/255,240/255,1.0); gl.enable(gl.DEPTH_TEST);
       // Compile vertex and fragment shader, return program.
@@ -710,7 +710,7 @@
       static graphGL(f,options) {
       // Create a canvas, webgl2 context and set some default GL options.
         var canvas=document.createElement('canvas'); canvas.style.width=options.width||''; canvas.style.height=options.height||''; canvas.style.backgroundColor='#EEE';
-        if (options.width && options.width.match(/px/i)) canvas.width = parseFloat(options.width); if (options.height && options.height.match(/px/i)) canvas.height = parseFloat(options.height);
+        if (options.width && options.width.match && options.width.match(/px/i)) canvas.width = parseFloat(options.width); if (options.height && options.height.match && options.height.match(/px/i)) canvas.height = parseFloat(options.height);
         var gl=canvas.getContext('webgl',{alpha:options.alpha||false,antialias:true,preserveDrawingBuffer:options.still||true,powerPreference:'high-performance'}); 
         gl.enable(gl.DEPTH_TEST); gl.depthFunc(gl.LEQUAL); if (!options.alpha) gl.clearColor(240/255,240/255,240/255,1.0); gl.getExtension("OES_standard_derivatives"); gl.va=gl.getExtension("OES_vertex_array_object");
       // Compile vertex and fragment shader, return program.  
