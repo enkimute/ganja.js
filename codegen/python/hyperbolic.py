93ef8301a27cf97d2511384ab1b60935ee865e36
Martin Roelfs <u0114255@kuleuven.be>
2019-07-18 11:53:45 +0200

    Regenerated all standard source files

--- codegen/python/hyperbolic.py
+++ codegen/python/hyperbolic.py
@@ -1,193 +1,179 @@
-## 3D Projective Geometric Algebra
-## Written by a generator written by enki.
+"""3D Projective Geometric Algebra.
+
+Written by a generator written by enki.
+"""
+
+__author__ = 'Enki'
+
 import math
+
 class HYPERBOLIC:
-	def __init__(self, v=0, i=0):
-		self.mvec = [0]*2
-		self._base = ["1","e1"]
-		if (v!=0):
-			self.mvec[i] = v
+    def __init__(self, value=0, index=0):
+        """Initiate a new HYPERBOLIC.
+         
+        Optional, the component index can be set with value.
+        """
+        self.mvec = [0] * 2
+        self._base = ["1", "e1"]
+        if (value != 0):
+            self.mvec[index] = value
         
-	def __str__(self):
-		res = ' + '.join(filter(None, [("%.7f" % x).rstrip("0").rstrip(".")+(["",self._base[i]][i>0]) if math.fabs(x) > 0.000001 else None for i,x in enumerate(self.mvec)]))
-		if (res == ''):
-			return "0"
-		return res
+    def __str__(self):
+        res = ' + '.join(filter(None, [("%.7f" % x).rstrip("0").rstrip(".")+(["",self._base[i]][i>0]) if math.fabs(x) > 0.000001 else None for i,x in enumerate(self)]))
+        if (res == ''):
+            return "0"
+        return res
 
-	def __getitem__(self,key):
-		return self.mvec[key]
+    def __getitem__(self, key):
+        return self.mvec[key]
 
-	def __setitem__(self,key,value):
-		self.mvec[key]=value
+    def __setitem__(self, key, value):
+        self.mvec[key] = value
+        
+    def __len__(self):
+        return len(self.mvec)
 
-	########################## 
-	# HYPERBOLIC.Reverse
-	# Reverse the order of the basis blades.
-	########################## 
-	def __invert__(a):
-		res = HYPERBOLIC()
-		res[0]=a[0]
-		res[1]=a[1]
-		return res
+    def __invert__(a):
+        """HYPERBOLIC.Reverse
+        
+        Reverse the order of the basis blades.
+        """
+        res = HYPERBOLIC()
+        res[0]=a[0]
+        res[1]=a[1]
+        return res
 
-	########################## 
-	# HYPERBOLIC.Dual
-	# Poincare duality operator.
-	########################## 
-	def Dual(a):
-		res = HYPERBOLIC()
-		res[0]=a[1]
-		res[1]=a[0]
-		return res
+    def Dual(a):
+        """HYPERBOLIC.Dual
+        
+        Poincare duality operator.
+        """
+        res = HYPERBOLIC()
+        res[0]=a[1]
+        res[1]=a[0]
+        return res
 
-	########################## 
-	# HYPERBOLIC.Conjugate
-	# Clifford Conjugation
-	########################## 
-	def Conjugate(a):
-		res = HYPERBOLIC()
-		res[0]=a[0]
-		res[1]=-a[1]
-		return res
+    def Conjugate(a):
+        """HYPERBOLIC.Conjugate
+        
+        Clifford Conjugation
+        """
+        res = HYPERBOLIC()
+        res[0]=a[0]
+        res[1]=-a[1]
+        return res
 
-	########################## 
-	# HYPERBOLIC.Involute
-	# Main involution
-	########################## 
-	def Involute(a):
-		res = HYPERBOLIC()
-		res[0]=a[0]
-		res[1]=-a[1]
-		return res
+    def Involute(a):
+        """HYPERBOLIC.Involute
+        
+        Main involution
+        """
+        res = HYPERBOLIC()
+        res[0]=a[0]
+        res[1]=-a[1]
+        return res
 
-	########################## 
-	# HYPERBOLIC.Mul
-	# The geometric product.
-	##########################
-	def __mul__(a,b):
-		if type(b) in (int,float):
-			 return a.muls(b)
-		res = HYPERBOLIC()
-		res[0]=b[0]*a[0]+b[1]*a[1]
-		res[1]=b[1]*a[0]+b[0]*a[1]
-		return res
-	__rmul__=__mul__
+    def __mul__(a,b):
+        """HYPERBOLIC.Mul
+        
+        The geometric product.
+        """
+        if type(b) in (int, float):
+            return a.muls(b)
+        res = HYPERBOLIC()
+        res[0]=b[0]*a[0]+b[1]*a[1]
+        res[1]=b[1]*a[0]+b[0]*a[1]
+        return res
+    __rmul__=__mul__
 
-	########################## 
-	# HYPERBOLIC.Wedge
-	# The outer product. (MEET)
-	##########################
-	def __xor__(a,b):
-		res = HYPERBOLIC()
-		res[0]=b[0]*a[0]
-		res[1]=b[1]*a[0]+b[0]*a[1]
-		return res
-
-
-	########################## 
-	# HYPERBOLIC.Vee
-	# The regressive product. (JOIN)
-	##########################
-	def __and__(a,b):
-		res = HYPERBOLIC()
-		res[1]=b[1]*a[1]
-		res[0]=b[0]*a[1]+b[1]*a[0]
-		return res
+    def __xor__(a,b):
+        res = HYPERBOLIC()
+        res[0]=b[0]*a[0]
+        res[1]=b[1]*a[0]+b[0]*a[1]
+        return res
 
 
-	########################## 
-	# HYPERBOLIC.Dot
-	# The inner product.
-	##########################
-	def __or__(a,b):
-		res = HYPERBOLIC()
-		res[0]=b[0]*a[0]+b[1]*a[1]
-		res[1]=b[1]*a[0]+b[0]*a[1]
-		return res
+    def __and__(a,b):
+        res = HYPERBOLIC()
+        res[1]=b[1]*a[1]
+        res[0]=b[0]*a[1]+b[1]*a[0]
+        return res
 
 
-	########################## 
-	# HYPERBOLIC.Add
-	# Multivector addition
-	##########################
-	def __add__(a,b):
-		if type(b) in (int,float):
-			 return a.adds(b)
-		res = HYPERBOLIC()
-		res[0] = a[0]+b[0]
-		res[1] = a[1]+b[1]
-		return res
-	__radd__=__add__
-
-	########################## 
-	# HYPERBOLIC.Sub
-	# Multivector subtraction
-	##########################
-	def __sub__(a,b):
-		if type(b) in (int,float):
-			 return a.subs(b)
-		res = HYPERBOLIC()
-		res[0] = a[0]-b[0]
-		res[1] = a[1]-b[1]
-		return res
-	__rsub__=__sub__
-
-	########################## 
-	# HYPERBOLIC.smul
-	# scalar/multivector multiplication
-	##########################
-	def smul(a,b):
-		res = HYPERBOLIC()
-		res[0] = a*b[0]
-		res[1] = a*b[1]
-		return res
+    def __or__(a,b):
+        res = HYPERBOLIC()
+        res[0]=b[0]*a[0]+b[1]*a[1]
+        res[1]=b[1]*a[0]+b[0]*a[1]
+        return res
 
 
-	########################## 
-	# HYPERBOLIC.muls
-	# multivector/scalar multiplication
-	##########################
-	def muls(a,b):
-		res = HYPERBOLIC()
-		res[0] = a[0]*b
-		res[1] = a[1]*b
-		return res
+    def __add__(a,b):
+        """HYPERBOLIC.Add
+        
+        Multivector addition
+        """
+        if type(b) in (int, float):
+            return a.adds(b)
+        res = HYPERBOLIC()
+        res[0] = a[0]+b[0]
+        res[1] = a[1]+b[1]
+        return res
+    __radd__=__add__
+
+    def __sub__(a,b):
+        """HYPERBOLIC.Sub
+        
+        Multivector subtraction
+        """
+        if type(b) in (int, float):
+            return a.subs(b)
+        res = HYPERBOLIC()
+        res[0] = a[0]-b[0]
+        res[1] = a[1]-b[1]
+        return res
+    __rsub__=__sub__
+
+    def smul(a,b):
+        res = HYPERBOLIC()
+        res[0] = a*b[0]
+        res[1] = a*b[1]
+        return res
 
 
-	########################## 
-	# HYPERBOLIC.sadd
-	# scalar/multivector addition
-	##########################
-	def sadd(a,b):
-		res = HYPERBOLIC()
-		res[0] = a+b[0]
-		res[1] = b[1]
-		return res
+    def muls(a,b):
+        res = HYPERBOLIC()
+        res[0] = a[0]*b
+        res[1] = a[1]*b
+        return res
 
 
-	########################## 
-	# HYPERBOLIC.adds
-	# multivector/scalar addition
-	##########################
-	def adds(a,b):
-		res = HYPERBOLIC()
-		res[0] = a[0]+b
-		res[1] = a[1]
-		return res
+    def sadd(a,b):
+        res = HYPERBOLIC()
+        res[0] = a+b[0]
+        res[1] = b[1]
+        return res
 
 
-	def norm(a):
-		return math.sqrt(math.fabs((a*a.Conjugate())[0]))
+    def adds(a,b):
+        res = HYPERBOLIC()
+        res[0] = a[0]+b
+        res[1] = a[1]
+        return res
 
-	def inorm(a):
-		return a.Dual().norm()
-		
-	def normalized(a):
-		return a*(1/a.norm())
 
-e1 = HYPERBOLIC(1.0,1)
+    def norm(a):
+        return math.sqrt(math.fabs((a * a.Conjugate())[0]))
+        
+    def inorm(a):
+        return a.Dual().norm()
+        
+    def normalized(a):
+        return a * (1 / a.norm())
 
-print("e1*e1         :", str(e1*e1))
-print("pss           :", str(e1))
-print("pss*pss       :", str(e1*e1))
+e1 = HYPERBOLIC(1.0, 1)
+
+if __name__ == '__main__':
+    print("e1*e1         :", str(e1*e1))
+    print("pss           :", str(e1))
+    print("pss*pss       :", str(e1*e1))
 
--- codegen/python/mink.py
+++ codegen/python/mink.py
@@ -1,223 +1,209 @@
-## 3D Projective Geometric Algebra
-## Written by a generator written by enki.
+"""3D Projective Geometric Algebra.
+
+Written by a generator written by enki.
+"""
+
+__author__ = 'Enki'
+
 import math
+
 class MINK:
-	def __init__(self, v=0, i=0):
-		self.mvec = [0]*4
-		self._base = ["1","e1","e2","e12"]
-		if (v!=0):
-			self.mvec[i] = v
+    def __init__(self, value=0, index=0):
+        """Initiate a new MINK.
+         
+        Optional, the component index can be set with value.
+        """
+        self.mvec = [0] * 4
+        self._base = ["1", "e1", "e2", "e12"]
+        if (value != 0):
+            self.mvec[index] = value
         
-	def __str__(self):
-		res = ' + '.join(filter(None, [("%.7f" % x).rstrip("0").rstrip(".")+(["",self._base[i]][i>0]) if math.fabs(x) > 0.000001 else None for i,x in enumerate(self.mvec)]))
-		if (res == ''):
-			return "0"
-		return res
+    def __str__(self):
+        res = ' + '.join(filter(None, [("%.7f" % x).rstrip("0").rstrip(".")+(["",self._base[i]][i>0]) if math.fabs(x) > 0.000001 else None for i,x in enumerate(self)]))
+        if (res == ''):
+            return "0"
+        return res
 
-	def __getitem__(self,key):
-		return self.mvec[key]
+    def __getitem__(self, key):
+        return self.mvec[key]
 
-	def __setitem__(self,key,value):
-		self.mvec[key]=value
+    def __setitem__(self, key, value):
+        self.mvec[key] = value
+        
+    def __len__(self):
+        return len(self.mvec)
 
-	########################## 
-	# MINK.Reverse
-	# Reverse the order of the basis blades.
-	########################## 
-	def __invert__(a):
-		res = MINK()
-		res[0]=a[0]
-		res[1]=a[1]
-		res[2]=a[2]
-		res[3]=-a[3]
-		return res
+    def __invert__(a):
+        """MINK.Reverse
+        
+        Reverse the order of the basis blades.
+        """
+        res = MINK()
+        res[0]=a[0]
+        res[1]=a[1]
+        res[2]=a[2]
+        res[3]=-a[3]
+        return res
 
-	########################## 
-	# MINK.Dual
-	# Poincare duality operator.
-	########################## 
-	def Dual(a):
-		res = MINK()
-		res[0]=a[3]
-		res[1]=-a[2]
-		res[2]=-a[1]
-		res[3]=a[0]
-		return res
+    def Dual(a):
+        """MINK.Dual
+        
+        Poincare duality operator.
+        """
+        res = MINK()
+        res[0]=a[3]
+        res[1]=-a[2]
+        res[2]=-a[1]
+        res[3]=a[0]
+        return res
 
-	########################## 
-	# MINK.Conjugate
-	# Clifford Conjugation
-	########################## 
-	def Conjugate(a):
-		res = MINK()
-		res[0]=a[0]
-		res[1]=-a[1]
-		res[2]=-a[2]
-		res[3]=-a[3]
-		return res
+    def Conjugate(a):
+        """MINK.Conjugate
+        
+        Clifford Conjugation
+        """
+        res = MINK()
+        res[0]=a[0]
+        res[1]=-a[1]
+        res[2]=-a[2]
+        res[3]=-a[3]
+        return res
 
-	########################## 
-	# MINK.Involute
-	# Main involution
-	########################## 
-	def Involute(a):
-		res = MINK()
-		res[0]=a[0]
-		res[1]=-a[1]
-		res[2]=-a[2]
-		res[3]=a[3]
-		return res
+    def Involute(a):
+        """MINK.Involute
+        
+        Main involution
+        """
+        res = MINK()
+        res[0]=a[0]
+        res[1]=-a[1]
+        res[2]=-a[2]
+        res[3]=a[3]
+        return res
 
-	########################## 
-	# MINK.Mul
-	# The geometric product.
-	##########################
-	def __mul__(a,b):
-		if type(b) in (int,float):
-			 return a.muls(b)
-		res = MINK()
-		res[0]=b[0]*a[0]+b[1]*a[1]-b[2]*a[2]+b[3]*a[3]
-		res[1]=b[1]*a[0]+b[0]*a[1]+b[3]*a[2]-b[2]*a[3]
-		res[2]=b[2]*a[0]+b[3]*a[1]+b[0]*a[2]-b[1]*a[3]
-		res[3]=b[3]*a[0]+b[2]*a[1]-b[1]*a[2]+b[0]*a[3]
-		return res
-	__rmul__=__mul__
+    def __mul__(a,b):
+        """MINK.Mul
+        
+        The geometric product.
+        """
+        if type(b) in (int, float):
+            return a.muls(b)
+        res = MINK()
+        res[0]=b[0]*a[0]+b[1]*a[1]-b[2]*a[2]+b[3]*a[3]
+        res[1]=b[1]*a[0]+b[0]*a[1]+b[3]*a[2]-b[2]*a[3]
+        res[2]=b[2]*a[0]+b[3]*a[1]+b[0]*a[2]-b[1]*a[3]
+        res[3]=b[3]*a[0]+b[2]*a[1]-b[1]*a[2]+b[0]*a[3]
+        return res
+    __rmul__=__mul__
 
-	########################## 
-	# MINK.Wedge
-	# The outer product. (MEET)
-	##########################
-	def __xor__(a,b):
-		res = MINK()
-		res[0]=b[0]*a[0]
-		res[1]=b[1]*a[0]+b[0]*a[1]
-		res[2]=b[2]*a[0]+b[0]*a[2]
-		res[3]=b[3]*a[0]+b[2]*a[1]-b[1]*a[2]+b[0]*a[3]
-		return res
-
-
-	########################## 
-	# MINK.Vee
-	# The regressive product. (JOIN)
-	##########################
-	def __and__(a,b):
-		res = MINK()
-		res[3]=b[3]*a[3]
-		res[2]=b[2]*a[3]+b[3]*a[2]
-		res[1]=b[1]*a[3]+b[3]*a[1]
-		res[0]=b[0]*a[3]+b[1]*a[2]-b[2]*a[1]+b[3]*a[0]
-		return res
+    def __xor__(a,b):
+        res = MINK()
+        res[0]=b[0]*a[0]
+        res[1]=b[1]*a[0]+b[0]*a[1]
+        res[2]=b[2]*a[0]+b[0]*a[2]
+        res[3]=b[3]*a[0]+b[2]*a[1]-b[1]*a[2]+b[0]*a[3]
+        return res
 
 
-	########################## 
-	# MINK.Dot
-	# The inner product.
-	##########################
-	def __or__(a,b):
-		res = MINK()
-		res[0]=b[0]*a[0]+b[1]*a[1]-b[2]*a[2]+b[3]*a[3]
-		res[1]=b[1]*a[0]+b[0]*a[1]+b[3]*a[2]-b[2]*a[3]
-		res[2]=b[2]*a[0]+b[3]*a[1]+b[0]*a[2]-b[1]*a[3]
-		res[3]=b[3]*a[0]+b[0]*a[3]
-		return res
+    def __and__(a,b):
+        res = MINK()
+        res[3]=b[3]*a[3]
+        res[2]=b[2]*a[3]+b[3]*a[2]
+        res[1]=b[1]*a[3]+b[3]*a[1]
+        res[0]=b[0]*a[3]+b[1]*a[2]-b[2]*a[1]+b[3]*a[0]
+        return res
 
 
-	########################## 
-	# MINK.Add
-	# Multivector addition
-	##########################
-	def __add__(a,b):
-		if type(b) in (int,float):
-			 return a.adds(b)
-		res = MINK()
-		res[0] = a[0]+b[0]
-		res[1] = a[1]+b[1]
-		res[2] = a[2]+b[2]
-		res[3] = a[3]+b[3]
-		return res
-	__radd__=__add__
-
-	########################## 
-	# MINK.Sub
-	# Multivector subtraction
-	##########################
-	def __sub__(a,b):
-		if type(b) in (int,float):
-			 return a.subs(b)
-		res = MINK()
-		res[0] = a[0]-b[0]
-		res[1] = a[1]-b[1]
-		res[2] = a[2]-b[2]
-		res[3] = a[3]-b[3]
-		return res
-	__rsub__=__sub__
-
-	########################## 
-	# MINK.smul
-	# scalar/multivector multiplication
-	##########################
-	def smul(a,b):
-		res = MINK()
-		res[0] = a*b[0]
-		res[1] = a*b[1]
-		res[2] = a*b[2]
-		res[3] = a*b[3]
-		return res
+    def __or__(a,b):
+        res = MINK()
+        res[0]=b[0]*a[0]+b[1]*a[1]-b[2]*a[2]+b[3]*a[3]
+        res[1]=b[1]*a[0]+b[0]*a[1]+b[3]*a[2]-b[2]*a[3]
+        res[2]=b[2]*a[0]+b[3]*a[1]+b[0]*a[2]-b[1]*a[3]
+        res[3]=b[3]*a[0]+b[0]*a[3]
+        return res
 
 
-	########################## 
-	# MINK.muls
-	# multivector/scalar multiplication
-	##########################
-	def muls(a,b):
-		res = MINK()
-		res[0] = a[0]*b
-		res[1] = a[1]*b
-		res[2] = a[2]*b
-		res[3] = a[3]*b
-		return res
+    def __add__(a,b):
+        """MINK.Add
+        
+        Multivector addition
+        """
+        if type(b) in (int, float):
+            return a.adds(b)
+        res = MINK()
+        res[0] = a[0]+b[0]
+        res[1] = a[1]+b[1]
+        res[2] = a[2]+b[2]
+        res[3] = a[3]+b[3]
+        return res
+    __radd__=__add__
+
+    def __sub__(a,b):
+        """MINK.Sub
+        
+        Multivector subtraction
+        """
+        if type(b) in (int, float):
+            return a.subs(b)
+        res = MINK()
+        res[0] = a[0]-b[0]
+        res[1] = a[1]-b[1]
+        res[2] = a[2]-b[2]
+        res[3] = a[3]-b[3]
+        return res
+    __rsub__=__sub__
+
+    def smul(a,b):
+        res = MINK()
+        res[0] = a*b[0]
+        res[1] = a*b[1]
+        res[2] = a*b[2]
+        res[3] = a*b[3]
+        return res
 
 
-	########################## 
-	# MINK.sadd
-	# scalar/multivector addition
-	##########################
-	def sadd(a,b):
-		res = MINK()
-		res[0] = a+b[0]
-		res[1] = b[1]
-		res[2] = b[2]
-		res[3] = b[3]
-		return res
+    def muls(a,b):
+        res = MINK()
+        res[0] = a[0]*b
+        res[1] = a[1]*b
+        res[2] = a[2]*b
+        res[3] = a[3]*b
+        return res
 
 
-	########################## 
-	# MINK.adds
-	# multivector/scalar addition
-	##########################
-	def adds(a,b):
-		res = MINK()
-		res[0] = a[0]+b
-		res[1] = a[1]
-		res[2] = a[2]
-		res[3] = a[3]
-		return res
+    def sadd(a,b):
+        res = MINK()
+        res[0] = a+b[0]
+        res[1] = b[1]
+        res[2] = b[2]
+        res[3] = b[3]
+        return res
 
 
-	def norm(a):
-		return math.sqrt(math.fabs((a*a.Conjugate())[0]))
+    def adds(a,b):
+        res = MINK()
+        res[0] = a[0]+b
+        res[1] = a[1]
+        res[2] = a[2]
+        res[3] = a[3]
+        return res
 
-	def inorm(a):
-		return a.Dual().norm()
-		
-	def normalized(a):
-		return a*(1/a.norm())
 
-e1 = MINK(1.0,1)
-e2 = MINK(1.0,2)
-e12 = MINK(1.0,3)
+    def norm(a):
+        return math.sqrt(math.fabs((a * a.Conjugate())[0]))
+        
+    def inorm(a):
+        return a.Dual().norm()
+        
+    def normalized(a):
+        return a * (1 / a.norm())
 
-print("e1*e1         :", str(e1*e1))
-print("pss           :", str(e12))
-print("pss*pss       :", str(e12*e12))
+e1 = MINK(1.0, 1)
+e2 = MINK(1.0, 2)
+e12 = MINK(1.0, 3)
+
+if __name__ == '__main__':
+    print("e1*e1         :", str(e1*e1))
+    print("pss           :", str(e12))
+    print("pss*pss       :", str(e12*e12))
 
