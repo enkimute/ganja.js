bd6bd793db9a05f59089bd7ca133ccb8830467e5
enkimute <enkimute@gmail.com>
2021-12-16 09:15:44 +0100

    fixes loading, scaling, notebooks

--- examples/coffeeshop.js
+++ examples/coffeeshop.js
@@ -12,16 +12,66 @@ else (function (){
   var _Algebra = window.Algebra;
   
   window.Algebra = function(...args) {
+    var container, id, res = _Algebra(...args);
     
-    console.log('_Algebra ', args);
-    
-    var res = _Algebra(...args);
-    
-    if (res instanceof HTMLCanvasElement || res instanceof SVGElement)
-      document.body.appendChild(res);
+    if (res && res.parentElement) {
+      res.parentElement.classList.add('trap');
+      id = res.parentElement.id;
+      if (document.body.children.length == 2) {
+        res.style.width = res.style.height = '100%';
+        document.body.overflow = 'hidden';
+      }
+    } else if (res) {
       
-    if (res && !(res instanceof Object))
-      document.body.innerHTML += `<PRE>${res+''}</PRE>`;
+      if (res instanceof HTMLCanvasElement || res instanceof SVGElement) {
+        if (!window.isNotebook) {
+          res.style.width = res.style.height = '100%';
+          document.body.appendChild(res);
+        } else {  
+          container = document.createElement('div');
+          container.classList.add('trap');
+          id = 'cont_'+(Math.random()*10000|0);
+          container.id = id;
+          container.appendChild(res);
+          document.body.appendChild(container);
+        } 
+      }
+      
+      if (res && res instanceof Array)
+        document.body.innerHTML += `<PRE>[${res+''}]</PRE>`;
+      else if (res && !(res instanceof Object))
+        document.body.innerHTML += `<PRE>${res+''}</PRE>`;
+    }    
+
+    if (window.inView && id) {
+      // Store original animation flag and turn everything of. 
+      if (res && res.options) { res.options.origAnimate = res.options.animate; res.options.animate = false; }
+      
+      // Now make sure in-view items are turned on.
+      inView('#'+id)
+          .on('enter', (x)=>{ 
+             x=x.firstChild; if(!x) return; 
+             if (x.options) {
+               if (x.options.origAnimate == undefined) x.options.origAnimate = x.options.animate;
+               x.options.animate=x.options.origAnimate; 
+               if (x.options.animate) {
+                 if (x.remake)  x=x.remake(true); 
+                 if (x.update)  x.update(x.value);
+                 console.log('svg on '+x.parentElement.id);  
+               }
+             }  
+          })
+          .on('exit', (x)=>{ 
+             x=x.firstChild; if(!x) return; 
+             if(x.options) {
+               x.options.animate=false; 
+               if (x.options.origAnimate) {
+                 if(x.remake) x=x.remake(false); 
+                 console.log('svg off '+x.parentElement.id); 
+               }  
+             }  
+          });         
+    }     
     
     return res;  
   }
@@ -48,6 +98,7 @@ else (function (){
   // Notebook option
   window.notebook = function(title) {
     window.title = title;
+    window.isNotebook = true;
     document.body.classList.add("notebook");
     document.body.parentElement.classList.add("notebook");
     marked.setOptions({
@@ -56,6 +107,5 @@ else (function (){
       }
     });
   }
-  
 
 })()
